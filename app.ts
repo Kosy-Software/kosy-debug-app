68a038150866bea77cec1c36d3c317ed8c87cc3b
MarkBracke <mark.bracke@tactics.be>
2021-03-07 18:01:34 +0100

    Updated messages for incoming and outgoing messages + first draft back-and-forth communication

--- app.ts
+++ app.ts
@@ -5,31 +5,34 @@ module Kozy {
         "integration-url": string
     }
 
+    type KosyClient = { 
+        info: ClientInfo,
+        iframe: HTMLIFrameElement,
+        makeHostButton: HTMLButtonElement 
+    }
+
+    type ClientMessage = any;
+
     export class KozyDebugger {
-        private clients: Array<{ identifier: string; iframe: HTMLIFrameElement; makeHostButton: HTMLButtonElement }> = [];
-        private hostClientIdentifier: string;
+        private clients: Array<KosyClient> = [];
         private addClientButton: HTMLButtonElement;
         private clientDiv: HTMLElement;
-
-        public sendOutgoingMessage (message: ServerToClientMessage, clientIdentifier: string) {
-            let matchingClient = this.clients.find(client => client.identifier = clientIdentifier);
-            if (matchingClient) {
-                matchingClient.iframe.contentWindow.postMessage(message, "*");
-            }
+        private defaultBuilding: Building = {
+            buildingKey: "TestBuilding",
+            buildingName: "TestBuilding"
         }
-
-        public receiveIncomingMessage (message: ClientToServerMessage) {
-            switch (message.type) {
-                case "ReadyAndListening":
-                    alert("Ready and listening.");
-                    break;
-                case "RelayToAllClients":
-                    alert("Relay to all hosts.");
-                    break;
-                case "RelayToHost":
-                    alert("Relay to host");
-                    break;
-            }
+        private defaultFloor: Floor = {
+            floorUuid: "TestFloor",
+            floorName: "TestFloor"
+        }
+        private defaultTable: Table = {
+            tableUuid: "TestTable",
+            tableName: "TestTable",
+            numberOfSeats: 999
+        }
+        private defaultRoom: Room = {
+            roomUuid: "TestRoom",
+            roomName: "TestRoom"
         }
 
         constructor () {
@@ -37,33 +40,110 @@ module Kozy {
             this.clientDiv = document.getElementById("clients");
         }
 
+        private findUnclaimedSeatNumber(): number {
+            var maxSeatNumber = 1;
+            for (var client of this.clients) {
+                switch (client.info.clientLocation.type) {
+                    case "SeatedAtTable":
+                        if (client.info.clientLocation.seatNumber > maxSeatNumber) {
+                            maxSeatNumber = client.info.clientLocation.seatNumber + 1;
+                        }
+                        break;
+                    case "InPrivateConversation":
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return maxSeatNumber;
+        }
+
+        private createClientHasJoinedMessage (client: KosyClient): ClientHasJoined {
+            return {
+                type: "ClientHasJoined",
+                payload: client.info
+            }
+        }
+
+        private createClientHasLeftMessage (client: KosyClient): ClientHasLeft {
+            return {
+                type: "ClientHasLeft",
+                payload: client.info
+            }
+        }
+
+        private createReceiveClientInfoMessage (kosyClient: KosyClient): ReceiveClientInfo {
+            return {
+                type: "ReceiveClientInfo",
+                payload: kosyClient.info
+            }
+        }
+
+        public sendOutgoingMessage (message: ServerToClientMessage<ClientMessage>, fromClient: KosyClient) {
+            fromClient.iframe.contentWindow.postMessage(message, "*");
+        }
+
+        public receiveIncomingMessage (message: ClientToServerMessage<ClientMessage>) {
+            switch (message.type) {
+                case "ReadyAndListening":
+                    alert("Received: Ready and listening.");
+                    break;
+                case "RelayMessage":
+                    alert("Received: Relay message.");
+                    break;
+            }
+        }
+
+        private unregisterClient(client: KosyClient): void {
+            this.clients = this.clients.filter(existing => existing = client);
+            this.sendOutgoingMessage(this.createClientHasLeftMessage (client), client);
+        }
+
+        private registerClient(kosyClient: KosyClient): void {
+            this.clients.push(kosyClient);
+            let receiveClientInfoMessage = this.createReceiveClientInfoMessage(kosyClient);
+            this.sendOutgoingMessage(receiveClientInfoMessage, kosyClient);
+            let clientHasJoinedMessage = this.createClientHasJoinedMessage(kosyClient)
+            this.clients.forEach(client => this.sendOutgoingMessage(clientHasJoinedMessage, client));
+            kosyClient.makeHostButton.onclick = event => {
+                this.unregisterClient(kosyClient);
+            };
+        }
+
+        private generateClientInfo(): ClientInfo {
+            let clientId = Date.now.toString();
+            return {
+                clientUuid: clientId,
+                clientName: "Client: " + clientId,
+                clientLocation: {
+                    type: "SeatedAtTable",
+                    building: this.defaultBuilding,
+                    floor: this.defaultFloor,
+                    room: this.defaultRoom,
+                    table: this.defaultTable,
+                    seatNumber: this.findUnclaimedSeatNumber()
+                }
+            }
+        }
+
         private addNewClient (url: string): void {
             let iframeContainer = document.createElement("div");
             iframeContainer.style.display = "inline-grid";
             let iframe = document.createElement("iframe");
-            iframe.src = url;            
-            let identifier = Date.now().toString();
-            if (!this.clients) {
-                this.hostClientIdentifier = identifier;
-            }
+            iframe.src = url;
+            let info = this.generateClientInfo();
             iframeContainer.appendChild(iframe);
 
             let makeHostButton = document.createElement("button");
-            makeHostButton.innerHTML = "Make kozy host";
-            makeHostButton.onclick = event => {
-                this.hostClientIdentifier = identifier;
-                this.clients.forEach(client => {
-                    this.sendOutgoingMessage({ type: "HostHasChanged", payload: {} }, identifier);
-                });
-            };
+            makeHostButton.innerHTML = "Leave the table";
             iframeContainer.appendChild(makeHostButton);
             this.clientDiv.appendChild(iframeContainer);
 
-            this.clients.push({ identifier, iframe, makeHostButton });
+            this.registerClient({ info, iframe, makeHostButton });
         }
 
         public start (params: StartupParameters): void {
-            window.addEventListener("message", (event: MessageEvent<ClientToServerMessage>) => {
+            window.addEventListener("message", (event: MessageEvent<ClientToServerMessage<ClientMessage>>) => {
                 this.receiveIncomingMessage(event.data);
             });
             this.addClientButton.onclick = event => {
--- messages.d.ts
+++ messages.d.ts
@@ -1,35 +1,91 @@
 declare namespace Kozy {
     type HostHasChanged = {
-        type: "HostHasChanged"
-        payload: any
+        type: "HostHasChanged";
+        payload: any; //not known yet
+    }
+
+    type Building = {
+        buildingKey: string;
+        buildingName: string;
+    }
+
+    type Floor = {
+        floorUuid: string;
+        floorName: string
+    }
+
+    type Room = {
+        roomUuid: string;
+        roomName: string;
+    }
+
+    type Table = {
+        tableUuid: string;
+        tableName: string;
+        numberOfSeats: number;
+    }
+
+    type SeatedAtTable = {
+        type: "SeatedAtTable";
+        building: Building;
+        floor: Floor;
+        room: Room;
+        table: Table;
+        seatNumber: number;
+    }
+
+    type InPrivateConversation = {
+        type: "InPrivateConversation";
+        conversationKey: string;
+    }
+
+    type ClientLocation =
+        | SeatedAtTable
+        | InPrivateConversation
+
+    type ClientInfo = {
+        clientUuid: string;
+        clientName: string;
+        clientLocation: ClientLocation
     }
 
     type ReceiveClientInfo = {
         type: "ReceiveClientInfo"
-        payload: any
+        payload: ClientInfo
     }
+
+    type ReceiveMessage<T> = {
+        type: "ReceiveMessage";
+        payload: T;
+    }
+
+    type ClientHasLeft = {
+        type: "ClientHasLeft";
+        payload: ClientInfo
+    }
+
+    type ClientHasJoined = {
+        type: "ClientHasJoined";
+        payload: ClientInfo
+    }
+
+    type ServerToClientMessage<T> =
+        | ReceiveClientInfo
+        | ClientHasLeft
+        | ClientHasJoined
+        | ReceiveMessage<T>
 
     type ReadyAndListening = {
-        type: "ReadyAndListening"
-        payload: any
+        type: "ReadyAndListening";
+        payload: any; //not known yet
     }
 
-    type RelayToHost = {
-        type: "RelayToHost"
-        payload: any
+    type RelayMessage<T> = {
+        type: "RelayMessage"
+        payload: T
     }
 
-    type RelayToAllClients = {
-        type: "RelayToAllClients"
-        payload: any
-    }
-
-    type ServerToClientMessage =
-        | HostHasChanged
-        | ReceiveClientInfo
-
-    type ClientToServerMessage =
+    type ClientToServerMessage<T> =
         | ReadyAndListening
-        | RelayToHost
-        | RelayToAllClients
+        | RelayMessage<T>
 }
