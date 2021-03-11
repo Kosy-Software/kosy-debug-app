3fdec8b3e57f14933c2375483a8ffac078c736ed
MarkBracke <mark.bracke@tactics.be>
2021-03-11 18:21:04 +0100

    Declaring namespaces

--- frameworkmessages.d.ts
+++ frameworkmessages.d.ts
@@ -1,95 +1,97 @@
-type IntegrationToKosyMessage<T> =
-    | ReadyAndListening
-    | RelayMessage<T>
+declare namespace Kosy {
+    type IntegrationToKosyMessage<T> =
+        | ReadyAndListening
+        | RelayMessage<T>
 
-interface ReadyAndListening {
-    type: "ready-and-listening";
-    payload: any; //not known yet
-}
+    interface ReadyAndListening {
+        type: "ready-and-listening";
+        payload: any; //not known yet
+    }
 
-interface RelayMessage<T> {
-    type: "relay-message"
-    payload: T
-}
+    interface RelayMessage<T> {
+        type: "relay-message"
+        payload: T
+    }
 
-type KosyToIntegrationMessage<T> =
-    | ReceiveInitialInfo
-    | ClientHasJoined
-    | ClientHasLeft
-    | ReceiveMessage<T>
+    type KosyToIntegrationMessage<T> =
+        | ReceiveInitialInfo
+        | ClientHasJoined
+        | ClientHasLeft
+        | ReceiveMessage<T>
 
-/// Note: this message is also used when the client info has changed (e.g. seat number or name)
-interface ReceiveInitialInfo {
-    type: "receive-initial-info"
-    payload: InitialInfo
-}
+    /// Note: this message is also used when the client info has changed (e.g. seat number or name)
+    interface ReceiveInitialInfo {
+        type: "receive-initial-info"
+        payload: InitialInfo
+    }
 
-interface InitialInfo {
-    /// Information about all clients present
-    clients: { [clientUuid: string]: ClientInfo };
-    /// The current client's identifier
-    currentClientUuid: string;
-    /// The initializing client's identifier
-    initializerClientUuid: string;
-}
+    interface InitialInfo {
+        /// Information about all clients present
+        clients: { [clientUuid: string]: ClientInfo };
+        /// The current client's identifier
+        currentClientUuid: string;
+        /// The initializing client's identifier
+        initializerClientUuid: string;
+    }
 
-interface ClientInfo {
-    clientUuid: string;
-    clientName: string;
-    clientLocation: ClientLocation;
-}
+    interface ClientInfo {
+        clientUuid: string;
+        clientName: string;
+        clientLocation: ClientLocation;
+    }
 
-type ClientLocation =
-    | SeatedAtTable
-    | InPrivateConversation
+    type ClientLocation =
+        | SeatedAtTable
+        | InPrivateConversation
 
-interface SeatedAtTable {
-    type: "seated-at-table";
-    building: Building;
-    floor: Floor;
-    room: Room;
-    table: Table;
-    seatNumber: number;
-}
+    interface SeatedAtTable {
+        type: "seated-at-table";
+        building: Building;
+        floor: Floor;
+        room: Room;
+        table: Table;
+        seatNumber: number;
+    }
 
-interface InPrivateConversation {
-    type: "in-private-conversation";
-    conversationKey: string;
-}
+    interface InPrivateConversation {
+        type: "in-private-conversation";
+        conversationKey: string;
+    }
 
-interface Building {
-    buildingKey: string;
-    buildingName: string;
-}
+    interface Building {
+        buildingKey: string;
+        buildingName: string;
+    }
 
-interface Floor {
-    floorUuid: string;
-    floorName: string
-}
+    interface Floor {
+        floorUuid: string;
+        floorName: string
+    }
 
-interface Room {
-    roomUuid: string;
-    roomName: string;
-}
+    interface Room {
+        roomUuid: string;
+        roomName: string;
+    }
 
-interface Table {
-    tableUuid: string;
-    tableName: string;
-    numberOfSeats: number;
-}
+    interface Table {
+        tableUuid: string;
+        tableName: string;
+        numberOfSeats: number;
+    }
 
-/// Note: this message is also used when the client info has changed (e.g. seat number or name)
-interface ClientHasJoined {
-    type: "client-has-joined";
-    payload: ClientInfo
-}
+    /// Note: this message is also used when the client info has changed (e.g. seat number or name)
+    interface ClientHasJoined {
+        type: "client-has-joined";
+        payload: ClientInfo
+    }
 
-interface ClientHasLeft {
-    type: "client-has-left";
-    payload: ClientInfo
-}
+    interface ClientHasLeft {
+        type: "client-has-left";
+        payload: ClientInfo
+    }
 
-interface ReceiveMessage<T> {
-    type: "receive-message";
-    payload: T;
+    interface ReceiveMessage<T> {
+        type: "receive-message";
+        payload: T;
+    }
 }
