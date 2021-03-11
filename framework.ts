ccb603c39006f54b21b7e8e1191542f9e3e81e1a
MarkBracke <mark.bracke@tactics.be>
2021-03-11 18:06:39 +0100

    Made framework more OO'y

--- framework.ts
+++ framework.ts
@@ -1,3 +1,62 @@
+export type IntegrationToKosyMessage<T> =
+    | ReadyAndListening
+    | RelayMessage<T>
+
+export interface ReadyAndListening {
+    type: "ready-and-listening";
+    payload: any; //not known yet
+}
+
+export interface RelayMessage<T> {
+    type: "relay-message"
+    payload: T
+}
+
+export type KosyToIntegrationMessage<T> =
+    | ReceiveInitialInfo
+    | ClientHasJoined
+    | ClientHasLeft
+    | ReceiveMessage<T>
+
+/// Note: this message is also used when the client info has changed (e.g. seat number or name)
+export interface ReceiveInitialInfo {
+    type: "receive-initial-info"
+    payload: InitialInfo
+}
+
+export interface InitialInfo {
+    /// Information about all clients present
+    clients: { [clientUuid: string]: ClientInfo };
+    /// The current client's identifier
+    currentClientUuid: string;
+    /// The initializing client's identifier
+    initializerClientUuid: string;
+}
+
+export interface ClientInfo {
+    clientUuid: string;
+    clientName: string;
+    clientLocation: ClientLocation;
+}
+
+export type ClientLocation =
+    | SeatedAtTable
+    | InPrivateConversation
+
+export interface SeatedAtTable {
+    type: "seated-at-table";
+    building: Building;
+    floor: Floor;
+    room: Room;
+    table: Table;
+    seatNumber: number;
+}
+
+export interface InPrivateConversation {
+    type: "in-private-conversation";
+    conversationKey: string;
+}
+
 export interface Building {
     buildingKey: string;
     buildingName: string;
@@ -19,48 +78,10 @@ export interface Table {
     numberOfSeats: number;
 }
 
-export interface SeatedAtTable {
-    type: "seated-at-table";
-    building: Building;
-    floor: Floor;
-    room: Room;
-    table: Table;
-    seatNumber: number;
-}
-
-export interface InPrivateConversation {
-    type: "in-private-conversation";
-    conversationKey: string;
-}
-
-export type ClientLocation =
-    | SeatedAtTable
-    | InPrivateConversation
-
-export interface ClientInfo {
-    clientUuid: string;
-    clientName: string;
-    clientLocation: ClientLocation;
-}
-
-export interface InitialInfo {
-    /// Information about all clients that are currently participating in the integration
-    clients: { [clientUuid: string]: ClientInfo };
-    /// The current client's identifier
-    currentClientUuid: string;
-    /// The initializing client's identifier
-    initializerClientUuid: string;
-}
-
 /// Note: this message is also used when the client info has changed (e.g. seat number or name)
-export interface ReceiveInitialInfo {
-    type: "receive-initial-info"
-    payload: InitialInfo
-}
-
-export interface ReceiveMessage<T> {
-    type: "receive-message";
-    payload: T;
+export interface ClientHasJoined {
+    type: "client-has-joined";
+    payload: ClientInfo
 }
 
 export interface ClientHasLeft {
@@ -68,28 +89,7 @@ export interface ClientHasLeft {
     payload: ClientInfo
 }
 
-/// Note: this message is also used when the client info has changed (e.g. seat number or name)
-export interface ClientHasJoined {
-    type: "client-has-joined";
-    payload: ClientInfo
+export interface ReceiveMessage<T> {
+    type: "receive-message";
+    payload: T;
 }
-
-export type KosyToIntegrationMessage<T> =
-    | ReceiveInitialInfo
-    | ClientHasJoined
-    | ClientHasLeft
-    | ReceiveMessage<T>
-
-export interface ReadyAndListening {
-    type: "ready-and-listening";
-    payload: any; //not known yet
-}
-
-export interface RelayMessage<T> {
-    type: "relay-message"
-    payload: T
-}
-
-export type IntegrationToKosyMessage<T> =
-    | ReadyAndListening
-    | RelayMessage<T>
